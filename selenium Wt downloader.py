1cb5a965e6e4f7f622fd0eb03d4bcf6b615c12f4
Ratatou2 <Ratatou2@users.noreply.github.com>
2021-10-30 10:50:07 +0900

    셀레니움으로 이미지 링크 가져온 후 다운로드까지 테스트

--- selenium Wt downloader.py
+++ selenium Wt downloader.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+import requests
 import time
+
+fake_header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
 driver = webdriver.Chrome("chromedriver.exe")
 driver.get('https://comic.naver.com/webtoon/detail?titleId=650305&no=335&weekday=sat')
@@ -16,8 +19,18 @@ for link in web_elements:
 print(trans_texts)
 raw_link = img_list[0].split('comic content')
 
+cook_link = []
 for link in raw_link:
     if 'img src' in link:
+        # 웹 링크로 이미지를 다운로드할 때 양쪽에 "(따옴표)는 빼줘야 함
         left = link.find('img src')
         right = link.find(' title')
-        print(link[left+8:right])
+        cook_link.append(link[left+9:right-1])
+
+count = 0
+for i in cook_link:
+    ep_html = requests.get(i, headers=fake_header)
+    with open(str(count) + '.png', 'wb+') as f:
+        f.write(ep_html.content)
+
+    count += 1
